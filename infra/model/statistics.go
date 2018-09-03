35125557924b4b3116e38894953a0f15538da162
wangpengcheng <wangpengcheng@ccssoft.com.cn>
2018-09-03 13:49:10 +0800

    法国队、克罗地亚队晋级之路

--- infra/model/statistics.go
+++ infra/model/statistics.go
@@ -6,15 +6,15 @@ import (
 )
 
 // https://www.fifa.com/worldcup/statistics/teams/goal-scored
-// 统计球队－--TOP GOALS
+// 统计球队－--Top goals
 type TeamStatisticWithTopGoal struct {
 	gorm.Model           //
 	Rank          int    `gorm:"type:integer;not null;column:rank"`           //排名
 	TeamName      string `gorm:"type:varchar(12);not null;column:team_name"`  //球队名
 	MatchPlayed   int    `gorm:"type:integer;not null;column:matches_played"` //球队打的比赛场次
-	GoalsFor      int    `gorm:"type:integer;not null;column:goals_for"`      //球队总进球数
+	GoalsFor      int    `gorm:"type:integer;not null;column:goals_for"`      //球队总进球数(包括加时进球数)
 	GoalsScored   int    `gorm:"type:integer;not null;column:goals_scored"`   //
-	GoalsAgainst  int    `gorm:"type:integer;not null;column:goals_against"`  //
+	GoalsAgainst  int    `gorm:"type:integer;not null;column:goals_against"`  //球队总的失球数(包括加时赛丢球数)
 	PenaltyGoal   int    `gorm:"type:integer;not null;column:penalty_goal"`   //罚球目标
 	OwnGoalsFor   int    `gorm:"type:integer;not null;column:own_goals_for"`  //自己的目标
 	OpenPlayGoals int    `gorm:"type:integer;not null;column:open_play_goals"`
